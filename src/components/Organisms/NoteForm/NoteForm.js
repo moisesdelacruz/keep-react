0124704962f0ee6d7eb74d3c0f87f25369a2f4b4
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-09-14 14:12:48 -0500

    ADD_NOTE action

--- src/components/Organisms/NoteForm/NoteForm.js
+++ src/components/Organisms/NoteForm/NoteForm.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import uuidv1 from 'uuid/v1'
 
-import Row from 'src/components/Atoms/Row'
+// import Row from 'src/components/Atoms/Row'
 import Icon from 'src/components/Atoms/Icon'
 
 import styles from './NoteFormStyles.sass'
@@ -19,12 +19,20 @@ class NoteForm extends Component {
     className: ''
   }
 
-  addNewNote = () => {
+  addNewNote = (event) => {
+    event.preventDefault()
+    // get form and prepare object for the action
+    const $form = document.getElementById('form')
+    const form = new FormData($form)
     const data = {
       id: uuidv1(),
-      title: 'Test',
-      description: 'Lorem Ipsum'
+      title: form.get('title'),
+      description: form.get('description')
     }
+    // clear form
+    $form.children.title.value = ''
+    $form.children.description.value = ''
+    // dispatch action
     this.props.action(data)
   }
 
@@ -35,14 +43,14 @@ class NoteForm extends Component {
     })
 
     return (
-      <Row className={classNames}>
-        <input type="text" name="title" placeholder="Title" />
-        <textarea name="description" rows="4" placeholder="Description..." />
-        <button type="button" onClick={this.addNewNote}>
+      <form className={classNames} onSubmit={this.addNewNote} id="form">
+        <input type="text" name="title" placeholder="Title" required />
+        <textarea name="description" rows="4" placeholder="Description..." required />
+        <button type="submit">
           <Icon name="plus" style={{ paddingLeft: '7px', paddingRight: '7px', color: 'green' }} />
           Create
         </button>
-      </Row>
+      </form>
     )
   }
 }
