6264fad7029f00ec31b5cc2fc6519b8d718b76ba
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-09-20 13:59:29 -0500

    update action ADD_NOTE

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,11 +3,14 @@ import { createAction } from 'src/utils'
 export const SEARCH_NOTES = 'SEARCH_NOTES'
 export const ADD_NOTE = 'ADD_NOTE'
 export const REMOVE_NOTE = 'REMOVE_NOTE'
+export const EDIT_NOTE = 'EDIT_NOTE'
 
 const searchActions = createAction(SEARCH_NOTES)
 const addNoteActions = createAction(ADD_NOTE)
 const removeNoteActions = createAction(REMOVE_NOTE)
+const editNoteActions = createAction(REMOVE_NOTE)
 
 export const search = query => searchActions({ query })
 export const addNote = newNote => addNoteActions({ newNote })
 export const removeNote = id => removeNoteActions({ id })
+export const editNote = id => editNoteActions({ id })
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,7 @@
 import { cloneDeep } from 'lodash';
 
 import { SEARCH_NOTES, ADD_NOTE, REMOVE_NOTE } from 'src/store/actions'
-// import { setToState } from 'src/utils'
+import { setToState } from 'src/utils'
 import data from 'src/store/api.json'
 
 const initialState = {
@@ -12,12 +12,16 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SEARCH_NOTES: {
-      return state
+      const nextState = cloneDeep(state)
+      return nextState
     }
     case ADD_NOTE: {
-      const nextState = cloneDeep(state)
-      const data = nextState.data.data.concat({ ...action.payload.newNote })
-      return { data: { data } }
+      return setToState(state, {
+        'data.data': [
+          ...state.data.data,
+          action.payload.newNote
+        ]
+      })
     }
     case REMOVE_NOTE: {
       const toDelete = new Set([action.payload.id])
